40ec346f9a2e4a49612912a26484a5b6c48f86c7
Explorarthecreator <fredadejiemma99@gmail.com>
2024-02-29 17:49:01 +0100

    Added the Navbar provider from my navbar context. Also created a nav bar for the main element

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import Navbar from "./components/Navbar";
 import Room from "./pages/Room";
 import User from "./pages/User";
 import Lodge from "./pages/Lodge";
+import MainNav from "./components/MainNav";
+import { NavbarProvider } from "./context/navbarContext";
 function App() {
   return (
-    <div className=" flex w-full">
-      <Router>
-        <Navbar/>
-        <main className=" w-4/5 overflow-y-scroll h-screen overflow-x-hidden">
-          <Routes>
-            <Route path="/" element={<User/>}/>
-            <Route path="/room" element={<Room/>}/>
-            <Route path="/lodge" element={<Lodge/>}/>
-          </Routes>
-        </main>
-      </Router>
-      
-    </div>
+    <NavbarProvider>
+      <div className=" flex w-full">
+        <Router>
+          <Navbar/>
+          <main className=" w-full lg:w-4/5 overflow-y-scroll h-screen overflow-x-hidden bg-white">
+            <div className="w-full bg-slate-400 shadow-2xl">
+              <MainNav/>
+            </div>
+            <Routes>
+              <Route path="/" element={<User/>}/>
+              <Route path="/room" element={<Room/>}/>
+              <Route path="/lodge" element={<Lodge/>}/>
+            </Routes>
+          </main>
+        </Router>
+      </div>
+    </NavbarProvider>
   );
 }
 
