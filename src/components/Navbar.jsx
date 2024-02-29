c15da58d0b9a551fb6e3e967e1be849e31e6e388
Explorarthecreator <fredadejiemma99@gmail.com>
2024-02-29 15:16:29 +0100

    Styled the navbar. More to be done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom"
+import {FaUser, FaBuilding, FaBed} from 'react-icons/fa'
 function Navbar() {
     const navigate = useNavigate() 
     const location = useLocation()
@@ -9,30 +10,32 @@ function Navbar() {
         }
     }
   return (
-    <div className=" w-1/5 bg-orange-300 h-screen">
-        <nav className="navbarNav">
-            <ul className="navbarListItems">
-                <li className="navbarListItem" onClick={()=>navigate('/')}>
+        <nav className="w-1/5 h-screen bg-[#6a6969] text-[#323131] flex flex-col items-center py-9 gap-12">
+            <div className=" bg-red-500 w-28 h-28 rounded-full"></div>
+            <ul className=" w-2/3">
+                <li className={`cursor-pointer flex px-4 py-2 gap-4 items-center ${pathMatchRoute('/')?'bg-[#b4cd93] rounded-lg':''}`} onClick={()=>navigate('/')}>
                     {/* <ExploreIcon fill={pathMatchRoute('/')?'#2c2c2c':'#8f8f8f'} width={'36px'} height={'36px'}/> */}
-                    <p className={pathMatchRoute('/')?'navbarListItemNameActive':'navbarListItemName'}>
+                    <FaUser size={25}/>
+                    <p className=' font-medium'>
                         User
                     </p>
                 </li>
-                <li className="navbarListItem cursor-pointer" onClick={()=>navigate('/room')}>
+                <li className={`cursor-pointer flex px-4 py-2 gap-4 items-center ${pathMatchRoute('/room')?'bg-[#b4cd93] rounded-lg':''}`} onClick={()=>navigate('/room')}>
                     {/* <OfferIcon fill={pathMatchRoute('/offers')?'#2c2c2c':'#8f8f8f'} width={'36px'} height={'36px'}/> */}
-                    <p className={pathMatchRoute('/offers')?'navbarListItemNameActive':'navbarListItemName'}>
+                    <FaBed size={25}/>
+                    <p className='font-medium'>
                         Room
                     </p>
                 </li>
-                <li className="navbarListItem" onClick={()=>navigate('/lodge')}>
+                <li className={`cursor-pointer flex px-4 py-2 gap-4 items-center ${pathMatchRoute('/lodge')?'bg-[#b4cd93] rounded-lg':''}`} onClick={()=>navigate('/lodge')}>
+                    <FaBuilding size={25}/>
                     {/* <PersonOutlineIcon fill={pathMatchRoute('/profile')?'#2c2c2c':'#8f8f8f'} width={'36px'} height={'36px'}/> */}
-                    <p className={pathMatchRoute('/profile')?'navbarListItemNameActive':'navbarListItemName'}>
+                    <p className='font-medium'>
                         Lodge
                     </p>
                 </li>
             </ul>
         </nav>
-    </div>
   )
 }
 
