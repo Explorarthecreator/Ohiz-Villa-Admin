e0a80395716ecb26bfdeadb7d661eb9d7d8b939c
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-03 19:42:22 +0100

    Style: Responsiveness

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react'
+import { FaHamburger } from 'react-icons/fa'
 import navbarContext from '../context/navbarContext'
 
 function MainNav() {
     const {shownav, setShownav} = useContext(navbarContext)
     // const [displayNav, setDisplayNav] = useState(false)
   return (
-    <div>
-        <button className='btn block lg:hidden' onClick={()=>{
+    <div className='w-full h-16 lg:h-24 mb-2 lg:mb-6 flex items-center px-3'>
+        <button className='btn btn-outline block lg:hidden  ' onClick={()=>{
             console.log(shownav);
             setShownav(!shownav)
             }}>
-            Click me
+            <FaHamburger size={25} color='black'/>
         </button>
     </div>
   )
