456459cddeffddb08e2f13bb1a5b4787f0143eca
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-05 13:39:06 +0100

    Implementing the private route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Room from "./pages/Room";
 import User from "./pages/User";
 import Lodge from "./pages/Lodge";
 import Signin from "./pages/Signin";
+// import Ssd from "./pages/Ssd";
 import MainNav from "./components/MainNav";
+import PrivateRoute from "./components/PrivateRoute";
 import { NavbarProvider } from "./context/navbarContext";
 function App() {
   return (
@@ -15,8 +17,13 @@ function App() {
           <main className=" w-full lg:w-4/5 overflow-y-scroll h-screen overflow-x-hidden bg-white">
             <MainNav/>
             <Routes>
-              <Route path="/" element={<User/>}/>
-              <Route path="/room" element={<Room/>}/>
+              <Route path="/" element={<PrivateRoute/>}>
+                <Route path="/" element={<User/>}/>
+              </Route>
+              <Route path="/room" element={<PrivateRoute/>}>
+                <Route path="/room" element={<Room/>}/>
+              </Route>
+              {/* <Route path="/room" element={<Room/>}/> */}
               <Route path="/lodge" element={<Lodge/>}/>
               <Route path="/signin" element={<Signin/>}/>
             </Routes>
