270ee2004ac3ad8769c8af24585bb62dfe3fbe24
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-05 13:40:17 +0100

    Setting up the firebase config file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,24 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxxNP_4eVW71Hw-VAFhmJOKFguI4p3Vb4",
-  authDomain: "ohiz-villa.firebaseapp.com",
-  projectId: "ohiz-villa",
-  storageBucket: "ohiz-villa.appspot.com",
-  messagingSenderId: "125724798725",
-  appId: "1:125724798725:web:ba4e51ccdf221cf0b8916c",
-  measurementId: "G-M9PCBE27Z5"
+  apiKey: "AIzaSyBTKMmOTguFZuMRo1nd8S9Y8jMJ_P3PYmM",
+  authDomain: "ohiz-villa-app-aa204.firebaseapp.com",
+  projectId: "ohiz-villa-app-aa204",
+  storageBucket: "ohiz-villa-app-aa204.appspot.com",
+  messagingSenderId: "952291763167",
+  appId: "1:952291763167:web:6ac6152b2a9e1a79829a6a"
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore()
