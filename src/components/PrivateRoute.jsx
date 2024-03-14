1021c88380ef22be6a56ec9144de5696978a0614
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-14 14:10:43 +0100

    Brought in the spinner component  instead of the loading paragraph

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { useAuthStatus } from "../hooks/useAuthStatus"
+import Spinner from "./Spinner"
 const PrivateRoute = ()=> {
   const{loggedIn, checkingStatus} = useAuthStatus()
 // const loggedIn = false
+// const checkingStatus = true
 
   if(checkingStatus){
-    return <p>
-        Loading yen yen
-    </p>
+    return <Spinner/>
   }
 
   return loggedIn ? <Outlet/>: <Navigate to={'/signin'}/>
