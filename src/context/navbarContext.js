5ba6b270d721253c0ed5b10391699613316544ac
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-14 14:09:04 +0100

    Created a context to handle if a user is loggedin and also to show the nav bar

--- src/context/navbarContext.js
+++ src/context/navbarContext.js
@@ -1,13 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 const navbarContext = createContext()
 
 export const NavbarProvider = ({children})=>{
 
 
     const [shownav, setShownav] = useState(false)
-    
+    const [loggedIn, setLoggedIn] = useState()
+    const auth = getAuth()
+
+    useEffect(()=>{
+        onAuthStateChanged(auth,(user)=>{
+            if(user){
+              setLoggedIn(true)
+            }
+            else{
+              setLoggedIn(false)
+            //   console.log('Logged out');
+            }
+           })
+           // eslint-disable-next-line
+    },[])
     return <navbarContext.Provider value={{
         shownav,
+        loggedIn,
+        setLoggedIn,
         setShownav
     }}>
         {children}
