a2d0c35df5b1149d3f1b6d0e5c773336cdc43f28
Explorarthecreator <fredadejiemma99@gmail.com>
2024-03-21 10:55:53 +0100

    Created another type of spinner but it is in the sape of a box (bars)

---
+++ src/components/BoxSpinner.jsx
@@ -0,0 +1,10 @@
+
+function BoxSpinner() {
+  return (
+    <div className=" h-5/6 flex items-center justify-center mt-20">
+        <span className="loading loading-bars w-24 text-black"></span>
+    </div>
+  )
+}
+
+export default BoxSpinner
